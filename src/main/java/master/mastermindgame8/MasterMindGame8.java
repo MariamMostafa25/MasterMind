18042563c3d609d5c30f12187c2571896ecc2def
MariamMostafa25 <mm4483755@gmail.com>
2024-12-18 14:09:44 +0200

    create MasterMind project

---
+++ src/main/java/master/mastermindgame8/MasterMindGame8.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package master.mastermindgame8;
+
+
+public class MasterMindGame8 {
+
+    public static void main(String[] args) {
+       new Master8().show();
+    }
+}
